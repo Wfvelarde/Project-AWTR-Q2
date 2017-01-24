da87d7e9ad50b612beec28357f9d2cd00fd48c42
Austin Edwards <austintedwards@gmail.com>
2017-01-23 17:02:46 -0700

    Romp Create a single create.  Working on the refresh

--- test-features/romp-create/app.js
+++ test-features/romp-create/app.js
@@ -1,50 +1,65 @@
 $(function(){
-  var i=1;
-$("#create-romp"+i).click(function(){
-    event.preventDefault();
-      var romp = $("#rompname").val();
-      if (romp!==""){
-      $("#new-romp").append("<h5>"+romp+"</h5>"+
-      "<form id='member-form'>"+
-      "<ul id='memberlist'></ul>"+"<br>Otter Username:<br>"+
-      "<input id = 'membername' type='text' name='membername' value=''><br>"+
-      "</form>");
-    $("#create-romp"+i).remove();
-    $("#form").remove();
-    $(".button").append("<a id = 'addmember' class='waves-effect"+
-    " waves-teal btn-flat'>Add Otter</a>"+"<a id = 'confirmromp' class='waves-effect"+
-    " waves-teal btn-flat'>Confirm Romp</a>");
+var i=$("#romplist")[0].childNodes.length;
 
-        $("#addmember").click(function(){
-          event.preventDefault();
-          console.log("HEY");
-            var otter = $("#membername").val();
-            $("#memberlist").append(  "<li>"+otter+"</li>");
-            $("#membername").val("");
-          });
-          console.log(romp);
-        $("#confirmromp").click(function(){
-          i++;
-          event.preventDefault();
-          console.log("HEY");
-          $("#romplist").append("<a id = 'rompname' class='waves-effect"+
-          " waves-teal btn-flat'>"+romp+"</a><br>");
-          $(".romp-content").remove();
-          $("#romp-form").append("<div class='romp-content'>"+
-            "<form id='form'>"+
-              "Romp Name:<br>"+
-              "<input id = 'rompname' type='text' name='rompname' value=''><br>"+
-            "</form>"+
-            "<div id='new-romp'></div>"+
-            "<div class='button'>"+
-              "<a id = 'create-romp"+i+"' class='waves-effect waves-teal btn-flat'>Create Romp</a>"+
-            "</div>"+
-          "</div>");
-        });
+createRomp(i);
 
-    }
-  });
 
+function createRomp(i){
+  $("#create-romp"+i).click(function(){
+    console.log("Hey");
+      event.preventDefault();
+        var romp = $("#rompname").val();
+        if (romp!==""){
+          rompForm(romp);
+          addOtter();
+          confirm(romp);
+      }
+    });
+}
+
+
+function rompForm(romp){
+  $("#new-romp").append("<h5>"+romp+"</h5>"+
+  "<form id='member-form'>"+
+  "<ul id='memberlist'></ul>"+"<br>Otter Username:<br>"+
+  "<input id = 'membername' type='text' name='membername' value=''><br>"+
+  "</form>");
+$("#create-romp"+i).remove();
+$("#form").remove();
+$(".button").append("<a id = 'addmember' class='waves-effect"+
+" waves-teal btn-flat'>Add Otter</a>"+"<a id = 'confirmromp' class='waves-effect"+
+" waves-teal btn-flat'>Confirm Romp</a>");
+}
+
+  function addOtter(){
+    $("#addmember").click(function(){
+      event.preventDefault();
+        var otter = $("#membername").val();
+        $("#memberlist").append(  "<li>"+otter+"</li>");
+        $("#membername").val("");
+      });
+  }
+
+  function confirm(romp){
+    $("#confirmromp").click(function(){
+      event.preventDefault();
+      $("#romplist").append("<a id = 'rompname' class='waves-effect"+
+      " waves-teal btn-flat'>"+romp+"</a><br>");
+      $(".romp-content").remove();
+      $("#romp-form").append("<div class='romp-content'>"+
+        "<form id='form'>"+
+          "Romp Name:<br>"+
+          "<input id = 'rompname' type='text' name='rompname' value=''><br>"+
+        "</form>"+
+        "<div id='new-romp'></div>"+
+        "<div class='button'>"+
+          "<a id = 'create-romp"+i+"' class='waves-effect waves-teal btn-flat'>Create Romp</a>"+
+        "</div>"+
+      "</div>");
+    });
+    i=$("#romplist")[0].childNodes.length;
+    createRomp(i);
+  }
 
 
 });
