7d97f09833b535e2df2b17fec8b8f0cfc013a577
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-01-24 17:13:59 -0700

    fixed link to second page

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,5 @@ router.get('/page2', function(req, res, next) { //when you go to page2,
   res.render('page2'); //render page2
 });
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
